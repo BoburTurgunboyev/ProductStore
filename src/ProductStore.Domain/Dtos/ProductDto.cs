ba6ee453140b62e855acfdef65a4783778e88409
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-24 14:28:23 +0500

    Domain layer Added

---
+++ src/ProductStore.Domain/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductStore.Domain.Dtos
+{
+    public class ProductDto
+    {
+        [MaxLength(250)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ src/ProductStore.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStore.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(250)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
