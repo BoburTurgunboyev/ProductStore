3ba8579db2b9d6b7aef4d6f07d3abcea767af16d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-24 14:28:10 +0500

    Application layer added

---
+++ src/ProductStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductStore.Application.Services;
+using ProductStore.Domain.Dtos;
+
+namespace ProductStore.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAll()
+        {
+            var products = await _productService.RetrieveAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByName(string name)
+        {
+            var product = await _productService.RetrieveByNameAsync(name);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> Post(ProductDto productDto)
+        {
+            var product = await _productService.AddAsync(productDto);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> Put(Guid id, ProductDto productDto)
+        {
+            var product = await _productService.ModifyAsync(id, productDto);
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> Delete(Guid id) 
+        {
+            var product = await _productService.RemoveAsync(id);
+            return Ok(product);
+        }
+
+
+    }
+}
--- src/ProductStore.Api/Program.cs
+++ src/ProductStore.Api/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using ProductStore.Application.Services;
+using ProductStore.Infrastructure.Data;
+using ProductStore.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+//REpositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+// Services
+builder.Services.AddScoped<IProductService, ProductService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
