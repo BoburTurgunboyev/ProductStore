196b6f118e2704ef75cc1cecf2050e96b0081f9b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-02-24 14:27:44 +0500

    Application layer Added

---
+++ src/ProductStore.Application/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using ProductStore.Domain.Dtos;
+using ProductStore.Domain.Entities;
+
+namespace ProductStore.Application.Services
+{
+    public interface IProductService
+    {
+        public ValueTask<Product> AddAsync(ProductDto productDto);
+
+        public ValueTask<Product> ModifyAsync(Guid id, ProductDto productDto);
+
+        public ValueTask<Product> RemoveAsync(Guid id);
+
+        public ValueTask<Product?> RetrieveByNameAsync(string name);
+
+        public ValueTask<IList<Product>> RetrieveAllAsync();
+    }
+}
---
+++ src/ProductStore.Application/Services/ProductService.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ProductStore.Domain.Dtos;
+using ProductStore.Domain.Entities;
+using ProductStore.Infrastructure.Data;
+using ProductStore.Infrastructure.Repositories;
+
+namespace ProductStore.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _appDbContext;
+
+
+        public ProductService(IMapper mapper, IProductRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+
+        }
+
+        public async ValueTask<Product> AddAsync(ProductDto productDto)
+        {
+            if (productDto == null) throw new ArgumentException("The 'productDto' parameters cannot be null.");
+
+            try
+            {
+                var product = new Product()
+                {
+                    Name = productDto.Name,
+                    Description = productDto.Description,
+                };
+                await _repository.InsertAsync(product);
+                return product;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(" Product Could not created ", ex);
+            }
+
+        }
+
+        public async ValueTask<Product> RemoveAsync(Guid id)
+        {
+            if (id == null) throw new ArgumentNullException(nameof(id), "The 'id' parameter cannot be null.");
+
+            try
+            {
+                var product = await _repository.DeleteAsync(id);
+                return product;
+            }
+            catch
+            {
+                throw new ApplicationException($"Could not delete {id}");
+            }
+        }
+
+        public async ValueTask<IList<Product>> RetrieveAllAsync()
+        {
+            try
+            {
+                var product = await _repository.SelectAllAsync();
+                return product;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error retrieving products", ex);
+            }
+        }
+
+        public async ValueTask<Product?> RetrieveByNameAsync(string name)
+        {
+            if (name is null) throw new ArgumentNullException(nameof(name), "The 'name' parameter cannot be null.");
+            try
+            {
+                var product = await _repository.SelectByNameAsync(name);
+                return product;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error retrievbyNameing products", ex);
+            }
+        }
+
+        public async ValueTask<Product> ModifyAsync(Guid id, ProductDto productDto)
+        {
+            if (productDto == null || id == null) throw new ArgumentException("Both 'productDto' and 'id' parameters cannot be null.");
+            try
+            {
+                var product = await _repository.SelectByIdAsync(id);
+                product.Name = productDto.Name;
+                product.Description = productDto.Description;
+
+                var result = await _repository.UpdateAsync(id, product);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(" Product Could not created ", ex);
+            }
+        }
+    }
+}
